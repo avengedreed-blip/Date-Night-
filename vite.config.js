594e4742868a43175947a6d260667f5bf4b3dcb2
avengedreed-blip <avengedreed@gmail.com>
2025-10-08 00:39:14 +0000

    Update 2

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,25 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  }
+  build: {
+    target: "esnext",
+    cssMinify: true,
+    minify: "terser",
+    sourcemap: false,
+    outDir: "dist",
+    assetsInlineLimit: 8192,
+    terserOptions: {
+      compress: { drop_console: true, drop_debugger: true, passes: 2 },
+      format: { comments: false },
+    },
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  optimizeDeps: {
+    include: ["tone", "framer-motion"],
+  },
+  esbuild: {
+    legalComments: "none",
+  },
 });
